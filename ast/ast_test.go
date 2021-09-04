aa2fd38bc6a3fb019581df5de45812ef7488549c
Daniil Rutskiy <dstdfx@gmail.com>
2021-09-04 22:51:19 +0300

    Fix linter issue
    Signed-off-by: Daniil Rutskiy <dstdfx@gmail.com>

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -1,27 +1,28 @@
-package ast
+package ast_test
 
 import (
 	"testing"
 
+	"github.com/dstdfx/scroopy/ast"
 	"github.com/dstdfx/scroopy/token"
 )
 
 func TestString(t *testing.T) {
-	program := &Root{
-		Statements: []Statement{
-			&LetStatement{
+	program := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
 				Token: token.Token{
 					Type:    token.LET,
 					Literal: "let",
 				},
-				Name: &Identifier{
+				Name: &ast.Identifier{
 					Token: token.Token{
 						Type:    token.IDENT,
 						Literal: "myVar",
 					},
 					Value: "myVar",
 				},
-				Value: &Identifier{
+				Value: &ast.Identifier{
 					Token: token.Token{
 						Type:    token.IDENT,
 						Literal: "anotherVar",
