582c41263aa8fca936c571fd3e476a9fa6d95364
Daniil Rutskiy <dstdfx@gmail.com>
2021-09-23 17:47:35 +0300

    Print scroopy logo name
    Signed-off-by: Daniil Rutskiy <dstdfx@gmail.com>

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -8,11 +8,20 @@ import (
 	"github.com/dstdfx/scroopy/repl"
 )
 
+const scroopyASCIIName = `
+  ______ ___________  ____   ____ ______ ___.__.
+ /  ___// ___\_  __ \/  _ \ /  _ \\____ <   |  |
+ \___ \\  \___|  | \(  <_> |  <_> )  |_> >___  |
+/____  >\___  >__|   \____/ \____/|   __// ____|
+     \/     \/                    |__|   \/
+`
+
 func main() {
 	currentUser, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(scroopyASCIIName)
 	fmt.Printf("Hello %s! This is the Scroopy programming language!\n", currentUser.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
